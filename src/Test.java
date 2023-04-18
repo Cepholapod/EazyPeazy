23ab6fa55cf22e9447fb0b391aec11d3b411917b
camsona-url <Mewlover235@gmail.com>
2023-04-18 18:25:06 -0400

    modified to be a console recipe creator

--- src/Test.java
+++ src/Test.java
@@ -1,49 +1,29 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import java.io.File;
+import java.util.Scanner;
 
-/* camsona-url testing having a single class pull from other class's scenes
- * 
- */
-public class Test extends Application {
-	Stage stage1 = new Stage();
-	Recipe recipe = new Recipe();
-	@Override
-	public void start(Stage stage) throws Exception {
-		stage1 = stage;
-
-		stage.close();
-		
-		GridPane gpane = new GridPane();
-		
-		Button b1 = new Button("Recipe Maker");
-		b1.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-			Scene scene1 = new Scene(new RecipeMakerGUI(), 400,400);
-			stage1.setScene(scene1);
-		});
-//		Button b2 = new Button("Single Recipe");
-//		b2.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-//			Scene scene1 = new Scene(new SingleRecipeGUI(), 400,400);
-//			stage1.setScene(scene1);
-//		});
-		gpane.add(b1, 0, 0);
-//		gpane.add(b2, 1, 0);
-		
-		Scene scene = new Scene(gpane, 440,440);
-
-		stage1.setScene(scene);
-		
-		
-		stage1.show();
-	}
+public class Test {
 
 	public static void main(String[] args) {
-		launch();
+		Scanner sc = new Scanner(System.in);
+		
+		main.entrees = RecipeSelector.entreeSelector(new File("Entree.txt"));
+		main.sides = RecipeSelector.sideSelector(new File("Side.txt"));
+		main.desserts = RecipeSelector.dessertSelector(new File("Dessert.txt"));
+		
+		
+		
+		String input = "Y";
+		
+		while(input.equalsIgnoreCase("Y")) {
+			System.out.println("Entree #: " + main.entrees.size() + " Side #: " + main.sides.size() + " Dessert #: " + main.desserts.size());
+			
+			Recipe.createRecipe();
+			System.out.print("Would you like to make another? Y / N : ");
+			input = sc.next();
+		}
+		
+		RecipeSelector.shutdown();
+		sc.close();
 	}
-
-	
 
 }

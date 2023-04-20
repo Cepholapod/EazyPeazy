b355c01754b0557ad3a445a46e0a913500f56f1a
zharing02 <harin1zp@cmich.edu>
2023-04-19 20:29:27 -0400

    Added a User option to select dietary restrictions from checkbox. Still need to implement the logic

--- src/UserGUI.java
+++ src/UserGUI.java
@@ -1,9 +1,12 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -14,6 +17,9 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.Popup;
+
 
 public class UserGUI extends TilePane{
 	
@@ -23,7 +29,9 @@ public class UserGUI extends TilePane{
 	private String title;
 	private ImageView[] userImages = { new ImageView("UserImage/UserBlue.PNG"),new ImageView("UserImage/UserGreen.PNG"),
 			new ImageView("UserImage/UserOrange.PNG"),new ImageView("UserImage/UserPink.PNG")};
+	private String[] tags = {"carnivore","paleo","Mediterranean","dash","gf","lowFat","lactoseFre"};
 	private ComboBox<String> comboBox = new ComboBox<>();
+	private CheckBox checkBox;
 	User user = new User();
 	
 	public UserGUI (User user) {
@@ -44,13 +52,49 @@ public class UserGUI extends TilePane{
 		
 		Button recipeMakerButton = new Button("New Recipe"), displayButton = new Button("Display Recipe");
 		
+		VBox restrictionBar = new VBox();
+		restrictionBar.setSpacing(5);
+		restrictionBar.setPadding(new Insets(5));
+		for (String tag : tags) {
+			CheckBox checkBox = new CheckBox(tag);
+			restrictionBar.getChildren().add(checkBox);
+		}
 		
+		Button setRestrictions = new Button("Set Restrictions...");
+
+		setRestrictions.setOnAction(e -> {
+			Popup popup = new Popup();
+			popup.getContent().add(restrictionBar);
+            popup.setAutoHide(true);
+            
+            Window window = setRestrictions.getScene().getWindow();
+            // Get the screen coordinates of the button
+            Bounds bounds = setRestrictions.localToScreen(setRestrictions.getBoundsInLocal());
+
+            // Set the X and Y coordinates of the popup to position it directly under the button
+            popup.setX(bounds.getMinX());
+            popup.setY(bounds.getMaxY());
+
+            // Show the popup
+            popup.show(window);
+
+		});
+	     /*  VBox root = new VBox();
+	        root.setSpacing(10);
+	        root.setPadding(new Insets(10));
+	        root.getChildren().addAll(setRestrictions);
+	        Scene scene = new Scene(root, 300, 200);
+	        userWindow.setScene(scene);*/
+		
+		titleBar.getChildren().add(setRestrictions);
+				
 		getChildren().add(titleBar);
 		titleBar.getChildren().add(comboBox);
 		getChildren().add(backButton);
 		getChildren().add(nutritionButton);
 		getChildren().add(recipeMakerButton);
 		getChildren().add(displayButton);
+		getChildren().add(setRestrictions);
 		
 		//list of options for combo box 
 		ObservableList<String> options = FXCollections.observableArrayList(imageTitles);

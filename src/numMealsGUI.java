0c52f34e41494c687a03e3c262cac8c08a22f51e
jessi <jessi@35.32.54.144>
2023-04-21 18:39:07 -0400

    simply fix

--- src/numMealsGUI.java
+++ src/numMealsGUI.java
@@ -11,13 +11,13 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class numMealsGUI extends Pane{
+public class numMealsGUI extends Pane {
 	private GridPane pane = new GridPane();
 	private Label mealsLBL = new Label("Enter Number of Meals");
 	private TextField mealInputTF = new TextField();
 	private int numMeals;
 
-	public numMealsGUI(User user){
+	public numMealsGUI(User user) {
 		pane.setHgap(10);
 		pane.setVgap(10);
 		pane.setAlignment(Pos.CENTER);
@@ -27,37 +27,36 @@ public class numMealsGUI extends Pane{
 		mealsLBL.setFont(LBLfont);
 		pane.setStyle("-fx-background-color:  #aac4e8;");
 
-		mealInputTF.setOnKeyTyped(e -> numMeals+=Integer.parseInt(mealInputTF.getText()));
-	
 		Button exitBTN = new Button("Exit");
 		exitBTN.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
 			numMealsStage.close();
 		});
 		Button finishBTN = new Button("Show my Meals");
-		finishBTN.addEventHandler(MouseEvent.MOUSE_CLICKED, e-> {
+		finishBTN.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+			numMeals = Integer.parseInt(mealInputTF.getText());
 			MenuRecipeGUI gui = new MenuRecipeGUI(user, numMeals);
 		});
-		
-		//formatting
+
+		// formatting
 		mealsLBL.setAlignment(Pos.CENTER);
 		exitBTN.setAlignment(Pos.CENTER);
 		finishBTN.setAlignment(Pos.CENTER);
-		
+
 		for (int i = 0; i < 4; i++) {
 			RowConstraints row = new RowConstraints();
 			row.setPercentHeight(25);
 			pane.getRowConstraints().add(row);
 		}
-		
+
 		exitBTN.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		finishBTN.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-		
+
 		// setting up pane
 		pane.addRow(0, mealsLBL);
 		pane.addRow(1, mealInputTF);
 		pane.addRow(2, finishBTN);
 		pane.addRow(3, exitBTN);
-		
+
 		numMealsStage.setTitle("How many meals?");
 		numMealsStage.setScene(scene);
 		numMealsStage.show();
